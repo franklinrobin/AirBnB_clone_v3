386b34f87b0ee7c43589f8592232357871179770
Humberto Tejada <humtej1204@gmail.com>
2022-05-20 22:57:08 -0500

    Adding a handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ starts Flask
 from os import getenv
 from models import storage
 from api.v1.views import app_views
+import flask
 from flask import Flask
 
 
@@ -16,6 +17,13 @@ app.register_blueprint(app_views)
 def teardown_app(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    '''Return this page when a 404 error appear'''
+    data = {"error": "Not found"}
+    return flask.jsonify(data)
 
 
 if __name__ == '__main__':
